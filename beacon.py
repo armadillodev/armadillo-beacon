0e1bc12991fce50eae262596ca9bd0f0a118e9a9
Kai Dewey <deweykai5@gmail.com>
2020-07-15 17:13:57 +0900

    adjusted for modified api

--- beacon.py
+++ beacon.py
@@ -7,61 +7,78 @@ import random
 import time
 
 class Config:
-    def __init__(self, url, trailor_id):
+    def __init__(self, url, bikes):
         self.url = url
-        self.trailor_id = trailor_id
+        self.bikes = bikes
 
-def read_conf():
+def read_config():
     with open('config.json') as file:
         file_data = json.loads(file.read())
 
-        domain = file_data['domain']
-        api_path = file_data['api_path']
-        port = file_data['port']
+        servers = file_data['servers']
+        print(servers)
+        target = input('which server: ')
+
+        domain = servers[target]['domain']
+        api_path = servers[target]['api_path']
+        port = servers[target]['port']
 
         config = Config(
             f'http://{domain}:{port}{api_path}',
-            file_data['trailor_id']
+            file_data['bikes']
         )
         return config
 
 
-def make_packet(trailor_id, temp):
+def create_random_bike_data():
+    current = random.choice(range(10, 40))
+    voltage = random.choice(range(10, 40))
+    rpm = random.choice(range(10, 40))
+
     data = {
-        "trailor": trailor_id,
-        "temperature": temp,
+        "current": current,
+        "voltage": voltage,
+        "rpm": rpm,
     }
 
     return json.dumps(data)
 
-def send_data(url, data):
+def send_bike_data(url, bike_id, data):
     headers = {
         "Content-Type": "application/json",
     }
-    req = urllib.request.Request(url, data.encode('utf-8'), headers=headers)
-    try: 
+
+    url = f'{url}/bike/{bike_id}'
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data, headers=headers)
+
+    try:
         urllib.request.urlopen(req)
     except URLError as err:
         print('failed to send data')
         print(err)
 
-def get_random_temp():
-    return random.choice(range(10, 40))
+def create_random_bike_sender(url):
+    def send(bike_id):
+        data = create_random_bike_data()
+        print(f'sending: {data}')
+        send_bike_data(url, bike_id, data)
 
+    return send
 
 def main():
     print('starting armadillo beacon')
 
-    config = read_conf()
+    config = read_config()
 
     print(f'sending to: {config.url}')
-    print(f'trailor id: {config.trailor_id}')
+    print(f'bikes: {config.bikes}')
+
+    bike_sender = create_random_bike_sender(config.url)
 
     while True:
-        temp = get_random_temp()
-        data = make_packet(config.trailor_id, temp)
-        send_data(config.url, data)
-        print(f'sent: "{data}"')
+        for bike in config.bikes:
+            bike_sender(bike)
 
         time.sleep(1)
 
